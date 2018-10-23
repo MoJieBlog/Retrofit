e94a390937e9e097eb04059f899d2f114e28f790
lixiaopeng <lixiaopeng@mxtrip.cn>
2018-10-23 14:39:18 +0800

    添加同步请求

--- app/src/main/java/com/lzp/retrofit/net/NetURL.java
+++ app/src/main/java/com/lzp/retrofit/net/NetURL.java
@@ -2,8 +2,16 @@ package com.lzp.retrofit.net;
 
 import com.lzp.retrofit.TestModel;
 
+import java.util.Map;
+
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 /**
  * @author Li Xiaopeng
@@ -12,4 +20,43 @@ import retrofit2.http.GET;
 public interface NetURL {
     @GET("ajax.php?a=fy&f=auto&t=auto&w=hello%20world")
     Call<BaseNetModel<TestModel>> getCall();
+
+    /************************以下为模拟接口*************************/
+
+    /**
+     * 带参数的get请求
+     * @param parameter
+     * @return
+     */
+    @GET("text")
+    Call<BaseNetModel> TestWithParameter(@Query("parameter") String parameter);
+
+    /**
+     * 带多个参数的get请求
+     * @param parameter
+     * @return
+     */
+
+    @GET("text")
+    Call<BaseNetModel> TestWithMultiParameter(@Query("parameter") String parameter
+            ,@Query("parameter1") String parameter1);
+
+
+    /**
+     * 带多个参数的get请求
+     * @param parameter
+     * @return
+     */
+
+    @GET("text")
+    Call<BaseNetModel> TestWithMultiParameter(@QueryMap Map<String ,String> parameter);
+
+    /********************************表单请求*****************************************/
+    @FormUrlEncoded
+    @POST
+    Call<BaseNetModel> formPost(@Field("id") String id);
+
+    @FormUrlEncoded
+    @POST
+    Call<BaseNetModel> formPost(@FieldMap Map<String,String> map);
 }
--- app/src/main/java/com/lzp/retrofit/net/NetUtils.java
+++ app/src/main/java/com/lzp/retrofit/net/NetUtils.java
@@ -1,10 +1,13 @@
 package com.lzp.retrofit.net;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -29,7 +32,10 @@ public class NetUtils {
                     retrofit = new Retrofit.Builder()
                             .client(getNetClient())
                             .baseUrl(BASE_URL)
-                            .addConverterFactory(GsonConverterFactory.create())
+                            .addConverterFactory(GsonConverterFactory.create())//数据转换器工厂的集合
+                           // .addCallAdapterFactory()//网络请求适配器工厂的集合
+                           // .callbackExecutor()//回调方法执行器
+                            //.callFactory()//网络请求工厂
                             .build();
                 }
             }
@@ -48,7 +54,8 @@ public class NetUtils {
                 .readTimeout(5000, TimeUnit.MILLISECONDS)
                 .connectTimeout(5000, TimeUnit.MILLISECONDS)
                 .writeTimeout(5000, TimeUnit.MILLISECONDS)
-                //.addInterceptor(logInterceptor)//需要拦截器可调用此方法
+                //.addNetworkInterceptor()//需要网络拦截器可调用此方法
+                //.addInterceptor(logInterceptor)//添加应用拦截器
                // .cache(cache)//需要缓存可调用此方法
                 .build();
     }
@@ -57,7 +64,34 @@ public class NetUtils {
         E e = getRetrofit().create(clazz);
         return e;
     }
+
+    /**
+     * 异步请求
+     * @param request
+     * @param callback
+     * @param <T>
+     */
     public static<T> void doRequest(Call<T> request,Callback <T> callback){
         request.enqueue(callback);
+    }
+
+    /**
+     * 同步请求，不可以在主线程中使用
+     * @param request
+     * @param callback
+     * @param <T>
+     */
+    public static<T> void doExecute(Call<T> request,Callback <T> callback){
+        try {
+            Response<T> execute = request.execute();
+            boolean successful = execute.isSuccessful();
+            if (successful){
+                callback.onResponse(request,execute);
+            }else{
+                callback.onFailure(request,new RuntimeException(execute.message()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
